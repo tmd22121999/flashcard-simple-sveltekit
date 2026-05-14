68f3eeb0fd6e41a6f9e78e4a9d929e6c9fe790ec
tmd22121999 <tmdtmd22121999@gmail.com>
2026-05-14 17:24:20 +0700

    Feat: change to build static

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  export const ssr = false;
+  export const prerender = true;
   import '../app.css';
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
 	preprocess: vitePreprocess(),  // ← required for lang="ts"
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production'
 				? '/flashcard-simple-sveltekit'
